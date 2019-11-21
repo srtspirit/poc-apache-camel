573422d33d5630951b38440c61478573ee402370
Vlasov, Andrey <andrey.vlasov@sap.com>
2019-11-21 09:49:11 -0500

    retry policy attemt

--- src/main/java/com/sap/cx/poc/camel/beans/Beans.java
+++ src/main/java/com/sap/cx/poc/camel/beans/Beans.java
@@ -1,6 +1,8 @@
 package com.sap.cx.poc.camel.beans;
 
 import org.apache.camel.component.amqp.AMQPComponent;
+
+import org.apache.camel.processor.RedeliveryPolicy;
 import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,8 +23,24 @@ public class Beans
 	}
 
 	@Bean
+	public HttpComponent httpComponent()
+	{
+		return new HttpComponent();
+	}
+
+	@Bean
 	public AMQPComponent amqpComponent(JmsConnectionFactory jmsConnectionFactory)
 	{
 		return new AMQPComponent(jmsConnectionFactory);
+	}
+
+	@Bean("redelivery")
+	public RedeliveryPolicy redeliveryPolicy()
+	{
+		final RedeliveryPolicy redeliveryPolicy = new RedeliveryPolicy();
+		redeliveryPolicy.setMaximumRedeliveries(5);
+		redeliveryPolicy.setRedeliveryDelay(5000L);
+		redeliveryPolicy.setBackOffMultiplier(2D);
+		return redeliveryPolicy;
 	}
 }
--- src/main/java/com/sap/cx/poc/camel/routes/MyRoute.java
+++ src/main/java/com/sap/cx/poc/camel/routes/MyRoute.java
@@ -8,13 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRoute extends RouteBuilder
 {
-	TransactionAutoConfiguration transactionAutoConfiguration;
-
 	@Override
 	public void configure() throws Exception
 	{
 		from("custom:ec-session-enabled-queue")
 				.bean(new MyRoute())
-				.log("${body}");
+				.log("${body}")
+				.toD("rest:post:hybris-url/" + "${in.headers.tenantId}" + "/${in.headers.environmentId}?host=localhost:8081");
+
+		onException(RestletOperationException.class, HttpHostConnectException.class)
+				.redeliveryPolicyRef("redelivery") // should retry the failed step according to the policy (defined in bean) then proceed to this failure branch
+				.log("RestletOperationException")
+				.to("file:ec-retry-later-queues")
+				.handled(true);
 	}
 }
